cccb71d58c8ddf0ac123b630cdafe586060572ee
Comet Assistant <comet@example.com>
2025-10-03 05:04:20 +0000

    setup.js fixed

--- server/tests/setup.js
+++ server/tests/setup.js
@@ -1,25 +1,23 @@
 process.env.NODE_ENV = 'test';
 require('dotenv').config();
 
-const { connectDB, disconnectDB } = require('./src/config/database');
+const { connectDB, disconnectDB } = require('../src/config/database');
 const mongoose = require('mongoose');
 
 beforeAll(async () => {
   await connectDB();
 });
 
-afterAll(async () => {
-  // Clean up test data
-  if (mongoose.connection.db) {
-    await mongoose.connection.db.dropDatabase();
-  }
-  await disconnectDB();
-});
-
-// Clean between tests
 afterEach(async () => {
   const collections = mongoose.connection.collections;
   for (const key in collections) {
     await collections[key].deleteMany({});
   }
+});
+
+afterAll(async () => {
+  if (mongoose.connection.db) {
+    await mongoose.connection.db.dropDatabase();
+  }
+  await disconnectDB();
 });
